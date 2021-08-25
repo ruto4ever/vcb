874112be0511e2adff3c71eee19cac0501fa3b1c
ruto4ever <89509558+ruto4ever@users.noreply.github.com>
2021-08-25 11:17:58 +0400

    Add files via upload

---
+++ set_webhook.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4ddca574dde7df7b-45aa34b182943e0a-b6c20acfaee0b616';
+	$webhook = 'https://github.com/ruto4ever/vcb/blob/gh-pages';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
